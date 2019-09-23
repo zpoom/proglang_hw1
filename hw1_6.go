00ecfde21620bcc6fa5b11648c87bed0933745dd
zpoom <poom007.sk135@gmail.com>
2019-09-23 13:07:08 +0700

    edit hw1_6 pass by reference

--- hw1_6.go
+++ hw1_6.go
@@ -2,19 +2,23 @@ package main
 
 import "fmt"
 
-func incrementByReference(x *int){
-	*x++
-}
-
 func incrementByValue(x int){
-
+	x++
 }
-
+func incrementByReference(y []int){
+	y[0]++
+	y[1]++
+	y[2]++
+}
 func main(){
 	var x = 0
+	var y = []int{0,1,2}
+	y[0] = 0
+	y[1] = 1
+	y[2] = 2
 	fmt.Println("main : ",x)
 	incrementByValue(x)
 	fmt.Println("after call incrementByValue : ",x)
-	incrementByReference(&x)
-	fmt.Println("after call incrementByReference : ",x)
+	incrementByReference(y)
+	fmt.Println("after call incrementByReference : ",y)
 }
