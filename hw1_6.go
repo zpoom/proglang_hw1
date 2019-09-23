0c1ebad470e1fe0cd81ad7023478a3ede847cb4b
zpoom <poom007.sk135@gmail.com>
2019-09-23 13:28:20 +0700

    edit hw1_6 to pass by value only

--- hw1_6.go
+++ hw1_6.go
@@ -5,20 +5,14 @@ import "fmt"
 func incrementByValue(x int){
 	x++
 }
-func incrementByReference(y []int){
-	y[0]++
-	y[1]++
-	y[2]++
+func incrementByPointer(x *int){
+	*x++
 }
 func main(){
 	var x = 0
-	var y = []int{0,1,2}
-	y[0] = 0
-	y[1] = 1
-	y[2] = 2
 	fmt.Println("main : ",x)
 	incrementByValue(x)
-	fmt.Println("after call incrementByValue : ",x)
-	incrementByReference(y)
-	fmt.Println("after call incrementByReference : ",y)
+	fmt.Println("after increment by value : ",x)
+	incrementByPointer(&x)
+	fmt.Println("after increment by pointer : ",x)
 }
